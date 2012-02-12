6ddaa68952013b92db260a4fe561754deb9634d5
Alex Whittemore <alexwhittemore@gmail.com>
2012-02-12 11:12:19 -0500

    Initial commit - LCD + temperature functionality
    Currently, the program reads from 2 DS18B20 sensors and displays those
    temperatures on the LCD.

---
+++ FridgeTemp.ino
@@ -0,0 +1,84 @@
+
+/*
+  LiquidCrystal Library - Hello World
+ 
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the 
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+ 
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+ 
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ 
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+ 
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ */
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 10
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  // Start up the library
+  sensors.begin();
+  lcd.begin(16, 2);
+  lcd.print("Keg:    Ambient:");
+}
+
+void loop(void)
+{ 
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  //Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  //Serial.println("DONE");
+  
+  Serial.print("Temperature for the device 1 (F): ");
+  Serial.println(sensors.getTempFByIndex(0));
+  Serial.print("Temperature for the device 2 (F): ");
+  Serial.println(sensors.getTempFByIndex(1));
+  Serial.print("\n");
+  
+  lcd.setCursor(0, 1);
+  lcd.print(sensors.getTempFByIndex(0));
+  lcd.setCursor(8, 1);
+  lcd.print(sensors.getTempFByIndex(1));
+  
+  delay(1000);
+}
